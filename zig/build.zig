9d722a76b833eb0d388ccb3fa2ae9c6f3739ba82
Santtu Niskanen <mail@salamanteri.pro>
2026-03-15 16:18:17 +0200

    cleaned build files and fixed build to use correct library (for Zig 0.15.2)

--- zig/build.zig
+++ zig/build.zig
@@ -4,11 +4,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addSharedLibrary(.{
+    const lib = b.addLibrary(.{
         .name = "kvstore",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+        .linkage = .dynamic,
         .version = .{ .major = 0, .minor = 1, .patch = 0 },
     });
 
